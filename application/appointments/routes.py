2d8eedd66d1efb90f06e9faee2e0b9c5045d65b7
Adam Wood <adamwoodce@gmail.com>
2022-06-06 09:19:05 +0100

    Add docstrings and black format.

--- application/appointments/routes.py
+++ application/appointments/routes.py
@@ -8,9 +8,53 @@ from models.therapist import Therapist, Specialism
 from application.main import generate_response
 from application.auth.routes import auth_token_required
 
-@app.route('/get_appointments', methods=['GET'])
+
+@app.route("/get_appointments", methods=["GET"])
 @auth_token_required
 def get_appointments():
+    """Get appointments filtered by date, specialism or type.
+
+    URL
+    ----------
+    GET /get_appointments
+
+    Query Parameters
+    ----------
+    start :
+        A date formatted string defining the start date to check for appointments.
+    end :
+        A date formatted string defining the end date to check for appointments.
+    specialisms :
+        A comma separated list of therapist specialisms.
+    type :
+        The type of appointments to find.
+
+    Reponse
+    -------
+    400 :
+        No query parameters found.
+    200 :
+          Appointments found: {int}
+          Example :
+            {
+                "message": "Appointments found: 2.",
+                "appointments": [
+                    {
+                        "time": "2022-06-06 09:41:00",
+                        "duration": 60.0,
+                        "therapist": "John Smith",
+                        "type": "one-off",
+                    },
+                    {
+                        "time": "2022-06-06 09:41:00",
+                        "duration": 60.0,
+                        "therapist": "Jane Smith",
+                        "type": "one-off",
+                    },
+                ],
+            }
+
+    """
     args = request.args
     arg_keys = args.keys()
     # Return early if no query string params sent.
@@ -18,76 +62,120 @@ def get_appointments():
         return generate_response("No query parameters found.", 400)
 
     # If start and end dates passed, limit to that date range
-    if 'start' in arg_keys and 'end' in arg_keys:
-        start = html.escape(args['start'])
-        end = html.escape(args['end'])
+    if "start" in arg_keys and "end" in arg_keys:
+        start = html.escape(args["start"])
+        end = html.escape(args["end"])
     else:
         start = date(1970, 1, 1)
         end = datetime(2999, 12, 12)
 
     # If specialisms passed, find therapists with those specialisms only
-    if 'specialisms' in arg_keys:
-        specialisms = html.escape(args['specialisms']).split(',')
-        therapists = Therapist.query.filter(Therapist.specialisms.any(Specialism.name.in_(specialisms))).all()
+    if "specialisms" in arg_keys:
+        specialisms = html.escape(args["specialisms"]).split(",")
+        therapists = Therapist.query.filter(
+            Therapist.specialisms.any(Specialism.name.in_(specialisms))
+        ).all()
     else:
         therapists = Therapist.query.all()
 
     # If a specific type passed, use that, otherwise use the default options
-    if 'type' in arg_keys:
-        appt_type = [html.escape(args['type'])]
+    if "type" in arg_keys:
+        appt_type = [html.escape(args["type"])]
     else:
         appt_type = Appointment.types()
-    
+
     # Grab the list of appointments from the db according to filters
     appointments = Appointment.query.filter(
-        Appointment.start_datetime.between(start, end), 
-        Appointment.therapist_id.in_([x.id for x in therapists]), 
-        Appointment.appointment_type.in_(appt_type)).all()
+        Appointment.start_datetime.between(start, end),
+        Appointment.therapist_id.in_([x.id for x in therapists]),
+        Appointment.appointment_type.in_(appt_type),
+    ).all()
 
     # Parse the appointments so we have the correct format
     parsed_appointments = list(
         map(
             lambda x: {
-                'time': x.start_datetime,
-                'duration': (x.end_datetime - x.start_datetime) / timedelta(minutes=1),
-                'therapist': x.therapist.name,
-                'type': x.appointment_type
-                }, appointments
-            )
+                "time": x.start_datetime,
+                "duration": (x.end_datetime - x.start_datetime) / timedelta(minutes=1),
+                "therapist": x.therapist.name,
+                "type": x.appointment_type,
+            },
+            appointments,
         )
+    )
 
     # Return the parsed appointments list
     return generate_response(
-        f"Appointments found: {len(appointments)}", 
-        200, 
-        appointments=parsed_appointments)
+        f"Appointments found: {len(appointments)}",
+        200,
+        appointments=parsed_appointments,
+    )
 
 
-@app.route('/add_appointment', methods=['POST'])
+@app.route("/add_appointment", methods=["POST"])
 @auth_token_required
 def add_appointment():
+    """Add new appointment.
+
+    URL
+    ----------
+    POST /add_appointment
+
+    Query Parameters
+    ----------
+    start :
+        A datetime formatted string defining the start date of the appointment.
+    duration :
+        The duration of the appointment in minutes.
+    type :
+        The type of appointment to add.
+    therapist_id :
+        The id of the therapist to assign the appointment to.
+
+    Response
+    -------
+    400 :
+        Invalid query string values.
+    200 :
+          Appointment added.
+          Example :
+            {
+                "message": "Appointment added.",
+            }
+
+    """
     args = request.args
     arg_keys = args.keys()
     # Only accept complete requests
-    if 'start' not in arg_keys or 'duration' not in arg_keys or 'type' not in arg_keys or 'therapist_id' not in arg_keys:
-        return generate_response('Missing arguments. All of [start, duration, type, therapist_id] are required.', 400)
+    if (
+        "start" not in arg_keys
+        or "duration" not in arg_keys
+        or "type" not in arg_keys
+        or "therapist_id" not in arg_keys
+    ):
+        return generate_response(
+            "Missing arguments. All of [start, duration, type, therapist_id] are required.",
+            400,
+        )
 
-    if args['type'] not in Appointment.types():
-        return generate_response(f'Incorrect type. Must be one of {Appointment.types()}', 400)
+    if args["type"] not in Appointment.types():
+        return generate_response(
+            f"Incorrect type. Must be one of {Appointment.types()}", 400
+        )
 
-    therapist = Therapist.query.filter_by(id=html.escape(args['therapist_id'])).first()
+    therapist = Therapist.query.filter_by(id=html.escape(args["therapist_id"])).first()
     if therapist is None:
-        return generate_response('Therapist not found.', 400)
+        return generate_response("Therapist not found.", 400)
 
     # Convert string times to datetime compatible objects
     try:
-        start = datetime.strptime(args['start'], "%Y-%m-%d %H:%M")
-        duration = timedelta(minutes=int(args['duration']))
+        start = datetime.strptime(args["start"], "%Y-%m-%d %H:%M")
+        duration = timedelta(minutes=int(args["duration"]))
     except ValueError:
-        return generate_response('Invalid start time or duration.', 400)
+        return generate_response("Invalid start time or duration.", 400)
 
     # Actually add the appointment
-    appointment = Appointment(start, duration, args['type'], therapist)
+    appointment = Appointment(start, duration, args["type"], therapist)
     res = appointment.save()
 
     # Ensure correct status code returned depending on save status
@@ -96,5 +184,3 @@ def add_appointment():
     else:
         code = 400
     return generate_response(res, code)
-
-    
--- application/auth/routes.py
+++ application/auth/routes.py
@@ -10,17 +10,18 @@ from models.user import User
 
 def auth_token_required(f):
     """Decorator to secure endpoints."""
+
     @wraps(f)
     def wrapper(*args, **kwargs):
         try:
             # Get access token from header
-            auth_header = request.headers.get('Authorization')
-            access_token = auth_header.split(' ')[1]
+            auth_header = request.headers.get("Authorization")
+            access_token = auth_header.split(" ")[1]
         except AttributeError:
             # Occurs in event of no Authorisation header
-            return generate_response('Invalid Authorization token in header.', 401)
+            return generate_response("Invalid Authorization token in header.", 401)
         if access_token:
-            # Attempt to decode token for User ID           
+            # Attempt to decode token for User ID
             user_id = User.decode_token(access_token)
             if not isinstance(user_id, str):
                 return f(*args, **kwargs)
@@ -28,7 +29,9 @@ def auth_token_required(f):
                 # String user_id indicates invalid token
                 return generate_response(user_id, 401)
         return f(*args, **kwargs)
+
     return wrapper
+
 
 def get_args(data):
     try:
@@ -42,8 +45,36 @@ def get_args(data):
     except json.decoder.JSONDecodeError:
         return generate_response(f"Invalid JSON data supplied.", 400)
 
+
 @app.route("/register", methods=["POST"])
 def register():
+    """Register a new user.
+
+    URL
+    ----------
+    POST /register
+
+    Body Parameters
+    ----------
+    email :
+        New user email.
+    password :
+        New user password.
+
+    Response
+    -------
+    500:
+        Unknown error adding user.
+    400 :
+        Invalid credentials.
+    201 :
+          New user registered.
+          Example :
+            {
+                "message": "Successfully registered new user: test@test.com",
+            }
+
+    """
     # Get the request arguments
     args = get_args(request.json)
 
@@ -52,27 +83,57 @@ def register():
         return args
 
     # Ensure we have both email and password
-    if list(args.keys()) != ['email', 'password']:
-        return generate_response('email and/or password not passed with request.', 400)
+    if list(args.keys()) != ["email", "password"]:
+        return generate_response("email and/or password not passed with request.", 400)
 
     # Make sure that email and password are both string values
-    if not isinstance(args['email'], str) or not isinstance(args['password'], str):
-        return generate_response('Incorrect type for email and/or password.', 400)
+    if not isinstance(args["email"], str) or not isinstance(args["password"], str):
+        return generate_response("Incorrect type for email and/or password.", 400)
 
     # Add a new user
     try:
-        user = User(html.escape(args['email']))
-        user.set_password(html.escape(args['password']))
+        user = User(html.escape(args["email"]))
+        user.set_password(html.escape(args["password"]))
         user.save()
-        return generate_response(f'Successfully registered new user: {user.email}', 201)
+        return generate_response(f"Successfully registered new user: {user.email}", 201)
     except exc.IntegrityError:
-        return generate_response(f'User already exists.', 400)
+        return generate_response(f"User already exists.", 400)
     except Exception as e:
-        return generate_response( f'Error adding user.', 500)
+        return generate_response(f"Error adding user.", 500)
 
 
 @app.route("/login", methods=["POST"])
 def login():
+    """Request a token for a registered user.
+
+    URL
+    ----------
+    POST /login
+
+    Body Parameters
+    ----------
+    email :
+        New user email.
+    password :
+        New user password.
+
+    Response
+    -------
+    500:
+        Unknown error retrieving token.
+    401:
+        Unauthorized.
+    400 :
+        Missing credentials.
+    201 :
+          Token generated.
+          Example :
+            {
+                "message": "Token generated with 5 minute expiration.",
+                "token": "token"
+            }
+
+    """
     # Get request arguments
     args = get_args(request.json)
 
@@ -81,17 +142,21 @@ def login():
         return args
 
     # Ensure we have both email and password
-    if list(args.keys()) != ['email', 'password']:
-        return generate_response('email and/or password not passed with request.', 400)
-    
-    try:
-        user = User.query.filter_by(email=html.escape(args['email'])).first()
-        # If user not found or password is incorrect, unauthorized
-        if user is None or not user.check_password(html.escape(args['password'])):
-            return generate_response('Invalid email or password. Please try again.', 401)
+    if list(args.keys()) != ["email", "password"]:
+        return generate_response("email and/or password not passed with request.", 400)
 
-        # Generate and return token 
+    try:
+        user = User.query.filter_by(email=html.escape(args["email"])).first()
+        # If user not found or password is incorrect, unauthorized
+        if user is None or not user.check_password(html.escape(args["password"])):
+            return generate_response(
+                "Invalid email or password. Please try again.", 401
+            )
+
+        # Generate and return token
         token = user.generate_token()
-        return generate_response('Token generated with 5 minute expiration.', 200, token=token)
+        return generate_response(
+            "Token generated with 5 minute expiration.", 200, token=token
+        )
     except Exception as e:
-        return generate_response(f'Error retrieving token: {e}', 500)
+        return generate_response(f"Error retrieving token: {e}", 500)
